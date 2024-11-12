463bac45ed319a46a76071ed5b5a55b23203b95a
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2024-11-12 14:44:43 +0200

    final project2

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     ganache: {
-      url: "http://127.0.0.1:7545",
-      accounts: ["63a3e8be21c97fdc610ac9048460497abc770e51d7dc60317b70c01de3988703"],
+      url: "http://127.0.0.1:8545",
+      accounts: ["10929948d42a4adeecb170fd8cfb0cbbafd2d696f5f59df5244292db9db83215"],
     },
   },
 };
